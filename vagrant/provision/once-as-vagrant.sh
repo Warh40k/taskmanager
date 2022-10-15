878f042df17bd76b1e4a95232497921bfb068dec
Nikita <nicita.zinkevitch@yandex.ru>
2022-10-15 19:26:02 +0300

    Configured vagrant with docker (mydb + myadmin)

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -21,9 +21,9 @@ composer --no-progress --prefer-dist install
 info "Init project"
 ./init --env=Development --overwrite=y
 
-info "Apply migrations"
-./yii migrate --interactive=0
-./yii_test migrate --interactive=0
+#info "Apply migrations"
+#./yii migrate --interactive=0
+#./yii_test migrate --interactive=0
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
