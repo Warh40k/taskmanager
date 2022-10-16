130582f4aaf36fe56d617de1334625e6d284f195
Nikita <nicita.zinkevitch@yandex.ru>
2022-10-16 13:49:09 +0300

    Installed Twig

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'adminEmail' => 'kommunist.bear@gmail.com',
+    'supportEmail' => 'kommunist.bear@gmail.com',
+    'senderEmail' => 'kommunist.bear@gmail.com',
+    'senderName' => 'Nikita',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
 ];
