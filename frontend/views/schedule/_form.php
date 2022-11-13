d2d968fa0669afcfe83cdf107a871745ac91849d
nikita <nicita.zinkevitch@yandex.ru>
2022-11-13 11:30:53 +0300

    Добавление поля "Количество рабочих часов по умолчанию"

--- frontend/views/schedule/_form.php
+++ frontend/views/schedule/_form.php
@@ -6,13 +6,21 @@ use yii\widgets\ActiveForm;
 /** @var yii\web\View $this */
 /** @var common\models\Schedule $model */
 /** @var yii\widgets\ActiveForm $form */
+/** @var string $action */
 ?>
 
 <div class="schedule-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true])->label('Наименование расписания') ?>
+
+    <?php if($action == 'create'): ?>
+        <div class="form-group">
+            <?= Html::label('Количество рабочих часов по умолчанию', 'default_work_length', ['class' => 'control-label' ]) ?>
+            <?= Html::textInput('default_work_length', null, ['type' => 'number', 'class' => 'form-control']) ?>
+        </div>
+    <?php endif; ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
